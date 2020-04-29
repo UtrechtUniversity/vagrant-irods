d959af58f4c6b39ac5a1841bf92a139bb47ffdab
Sietse Snel <s.t.snel@uu.nl>
2020-04-29 14:33:58 +0200

    Disable IPv6 by default ...
    because having it enabled causes intermittent package installation
    failures on home connections with flaky IPv6 connectivity, and it
    typically isn't needed for testing iRODS features.

---
+++ _common/update-ipv6-settings.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+
+set -e
+set -o pipefail
+set -u
+
+export DEBIAN_FRONTEND=noninteractive
+
+SETTINGSFILE=${1:-./local-repo.env}
+
+if [ -f "$SETTINGSFILE" ]
+then source "$SETTINGSFILE"
+else echo "Error: settings file $SETTINGSFILE not found." && exit 1
+fi
+
+sudo sysctl -w "net.ipv6.conf.default.disable_ipv6=${IPV6_DISABLE}"
+sudo sysctl -w "net.ipv6.conf.all.disable_ipv6=${IPV6_DISABLE}"
+
+if [ -f /etc/centos-release ]
+then
+    SYSCTLCONF="/etc/sysctl.d/50-disable-ipv6.conf"
+    echo "net.ipv6.conf.default.disable_ipv6=${IPV6_DISABLE}" | sudo tee -a "$SYSCTLCONF"
+    echo "net.ipv6.conf.all.disable_ipv6=${IPV6_DISABLE}" | sudo tee -a "$SYSCTLCONF" 
+elif lsb_release -i | grep -q Ubuntu >& /dev/null
+then
+    # Need to disable IPv6 via Grub on Ubuntu, because values in
+    # sysctl.conf are not applied after reboot.
+    if [ "$IPV6_DISABLE" -eq "1" ]
+    then sudo perl -pi.bak -e \
+            's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' \
+            /etc/default/grub
+         sudo update-grub
+    fi  
+else
+  echo "Error: did not recognize distribution/image."
+  exit 1
+fi
